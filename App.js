476fb6d347496fb7f9e4771161bb3a8fcd50d0e6
Iñigo Martínez López <inigomlap@gmail.com>
2018-10-22 00:19:19 +0200

    Added Native-Hyperlink

--- App.js
+++ App.js
@@ -1,12 +1,19 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Hyperlink from 'react-native-hyperlink'
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
         <Text>Open up App.js to start working on your app!</Text>
-      </View>
+		
+		<Hyperlink linkStyle={ { color: '#2980b9', fontSize: 20 } } linkDefault={ true }>
+			<Text style={ { fontSize: 15 } }>
+			  This text will be parsed to check for clickable strings like https://open.spotify.com/track/00FRRwuaJP9KimukvLQCOz and made clickable.
+			</Text>
+		</Hyperlink>
+	  </View>
     );
   }
 }
