43c932a84be8cc67de2e80c1d2af09ed7271e386
imartinezl <inigomlap@gmail.com>
2019-01-14 23:17:16 +0100

    Agenda component updated

--- components/agenda.js
+++ components/agenda.js
@@ -7,23 +7,27 @@ export class AgendaC extends React.Component {
 		super(props);
 
 		this.state = {
-			
+			selected: selected
 		};
-
+		this.loadItems.bind(this);
 	}
 	componentDidMount(){
-		this.setState({
-			currentDate: '2012-05-22'
-		});
-		console.log('Component mount:');
-		console.log(this.state);
+		// this.setState({
+
+		// });
+		//console.log('Component mount:');
+		//console.log(this.state);
 
 	}
-	_onDayPress(day){
+	onDayPress(day){
 		console.log('day pressed:', day);
-		this.setState({
-			currentDate: day.dateString
+		this.setState({ selected: day.dateString }, () => {
+			console.log("State updated:",this.state);
+			this.loadItems(items);
 		});
+	}	
+	onDayChange(day){
+		console.log('day changed:', day);
 	}
 	renderItem(item, firstItemInDay) {
 	    return (
@@ -36,8 +40,8 @@ export class AgendaC extends React.Component {
 		);
 	}	
 	renderDay(day, item) {
-		console.log("DAY:")
-		console.log(day,item);
+		//console.log("DAY:")
+		//console.log(day,item);
 		if(typeof day !== "undefined"){
 			return (
 					<View style={styles.day}><Text>{day.dateString}</Text></View>
@@ -46,32 +50,35 @@ export class AgendaC extends React.Component {
 			return(<View />);
 		}
 	}
+	loadItems(items){
+		for(var key in items){
+			if(key === this.state.selected){
+				console.log(key, items[key]);
+			}
+		}
+
+	}
 	render() {
 		return (
 			<Agenda
 			  // the list of items that have to be displayed in agenda. If you want to render item as empty date
 			  // the value of date key kas to be an empty array []. If there exists no value for date key it is
 			  // considered that the date in question is not yet loaded
-			  items={
-			    {'2012-05-22': [{text: 'item 1 - any js object'}],
-			     '2012-05-23': [{text: 'item 2 - any js object'}],
-			     '2012-05-24': [],
-			     '2012-05-28': [{text: 'item 3 - any js object'},{text: 'any js object'}],
-			    }}
+			  items={items}
 			  // callback that gets called when items for a certain month should be loaded (month became visible)
 			  loadItemsForMonth={(month) => {console.log('trigger items loading')}}
 			  // callback that fires when the calendar is opened or closed
 			  onCalendarToggled={(calendarOpened) => {console.log(calendarOpened)}}
 			  // callback that gets called on day press
-			  onDayPress={this._onDayPress.bind(this)}
+			  onDayPress={this.onDayPress.bind(this)}
 			  // callback that gets called when day changes while scrolling agenda list
-			  onDayChange={(day)=>{console.log('day changed')}}
+			  onDayChange={this.onDayChange.bind(this)}
 			  // initially selected day
-			  selected={'2012-05-22'}
+			  selected={this.state.selected}
 			  // Minimum date that can be selected, dates before minDate will be grayed out. Default = undefined
-			  minDate={'2012-05-10'}
+			  minDate={minDate}
 			  // Maximum date that can be selected, dates after maxDate will be grayed out. Default = undefined
-			  maxDate={'2012-05-30'}
+			  maxDate={maxDate}
 			  // Max amount of months allowed to scroll to the past. Default = 50
 			  pastScrollRange={5}
 			  // Max amount of months allowed to scroll to the future. Default = 50
@@ -94,11 +101,7 @@ export class AgendaC extends React.Component {
 			  // Hide knob button. Default = false
 			  hideKnob={false}
 			  // By default, agenda dates are marked if they have at least one item, but you can override this if needed
-			  markedDates={{
-			    '2012-05-16': {selected: true, marked: true},
-			    '2012-05-17': {marked: true},
-			    '2012-05-18': {disabled: true}
-			  }}
+			  markedDates={markedDates}
 			  // If provided, a standard RefreshControl will be added for "Pull to Refresh" functionality. Make sure to also set the refreshing prop correctly.
 			  onRefresh={() => console.log('refreshing...')}
 			  // Set this true while waiting for new data from a refresh
@@ -106,12 +109,7 @@ export class AgendaC extends React.Component {
 			  // Add a custom RefreshControl component, used to provide pull-to-refresh functionality for the ScrollView.
 			  refreshControl={null}
 			  // agenda theme
-			  theme={{
-			    agendaDayTextColor: 'yellow',
-			    agendaDayNumColor: 'green',
-			    agendaTodayColor: 'red',
-			    agendaKnobColor: 'red'
-			  }}
+			  theme={theme}
 			  // agenda container style
 			  style={{}}
 			/>
@@ -139,3 +137,29 @@ const styles = StyleSheet.create({
     marginTop: 17
   }
 });
+
+
+var items = {
+	'2019-01-02': [{text: 'item 1 - any js object'},{link:'https://open.spotify.com/track/00FRRwuaJP9KimukvLQCOz'}],
+	'2019-01-03': [{text: 'item 2 - any js object'},{link:'https://open.spotify.com/track/00FRRwuaJP9KimukvLQCOz'}],
+	'2019-01-04': [],
+	'2018-12-28': [{text: 'item 3 - any js object'},{text: 'any js object'},{link:'https://open.spotify.com/track/00FRRwuaJP9KimukvLQCOz'}],
+};
+var markedDates = {
+	'2019-01-06': {selected: true, marked: true},
+	'2019-01-07': {marked: true},
+	'2019-01-08': {disabled: true}
+};
+const theme = {
+	agendaDayTextColor: 'yellow',
+	agendaDayNumColor: 'green',
+	agendaTodayColor: 'red',
+	agendaKnobColor: 'red',
+	dotColor: 'blue',
+	selectedDayBackgroundColor: 'blue',
+	backgroundColor: 'blue'
+};
+const minDate = '2018-12-10';
+const maxDate = '2019-02-10';
+const selected = Date();
+
