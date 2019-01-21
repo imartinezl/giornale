a1f38e14cf842769f8ed78598cb038b055e424b5
imartinezl <inigomlap@gmail.com>
2019-01-21 20:50:57 +0100

    Removed FirebaseInit

--- components/firebaseInit.js
+++
@@ -1,7 +0,0 @@
-// Initialize Firebase
-export const firebaseConfig = {
-  apiKey: "AIzaSyD-kMIFE5MWx1SLNSqQ3o-KE_Fp7_wRczY",
-  authDomain: "giornale-ac271.firebaseapp.com",
-  databaseURL: "https://giornale-ac271.firebaseio.com",
-  storageBucket: "giornale-ac271.appspot.com"
-};
