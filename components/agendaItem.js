872696f1df83431e97ac0df800828981eedb55e2
imartinezl <inigomlap@gmail.com>
2019-01-24 18:57:10 +0100

    New agendaItem style: ImageBackground component

--- components/agendaItem.js
+++ components/agendaItem.js
@@ -1,13 +1,16 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { Image, ImageBackground, View, Text, StyleSheet } from 'react-native';
+
+
 
 export class AgendaItem extends React.Component {
 	constructor(props){
 		super(props);
 
 		this.state = {
-
+		    color: '#ffffff',
 		};
+
 	}
 	componentDidMount() {
     	
@@ -15,34 +18,77 @@ export class AgendaItem extends React.Component {
 	componentWillUnmount() {
 
 	}
+	componentWillMount() {
+        console.log(this.props.item.albumImage);
+    }
+	// <Image
+	// 	style={styles.image}
+	// 	source={{uri: this.props.item.albumImage}}
+	// />
 	render() {
 		return (
+			<ImageBackground
+				style={styles.imageBackground}
+				source={{uri: this.props.item.albumImage}}
+				resizeMode={'cover'}
+				borderRadius={15}
+				blurRadius={4}
+				>
 			<View style={styles.item}>
-				<Text style={styles.title}>{this.props.item.title}</Text>
-				<Text>{this.props.item.artist}</Text>
-				<Text>{this.props.item.album}</Text>
+				<View style={[styles.textContainer,{backgroundColor: 'rgba(255,0,0,0.4)',}]}>
+					<Text style={styles.title}>{this.props.item.title}</Text>
+					<Text style={styles.artist}>{this.props.item.artist}</Text>
+					<Text style={styles.album}>{this.props.item.album}</Text>
+				</View>
 			</View>
+			</ImageBackground>
+
 		)
 	}
 }
 
 const styles = StyleSheet.create({
-  item: {
-    backgroundColor: 'white',
-    flex: 1,
-    borderRadius: 10,
+  imageBackground:{
+  	marginTop: 20,
+    marginBottom: 40,
     marginRight: 10,
-    marginTop: 17,
-    marginBottom: 150,
-    paddingTop: 10,
-  	paddingBottom: 10,
-  	paddingLeft: 15,
-  	paddingRight: 15,
-  	height: 200
+    width: 280, 
+    height: 280,
+  },
+  item: {
+    flex: 1,
+    backgroundColor: 'rgba(255,255,255,0.4)',
+    borderRadius: 15
+  },
+  textContainer:{
+  	height: 100,
+  	paddingTop: 20,
+  	paddingRight: 20,
+  	paddingBottom: 20,
+  	marginTop: 'auto',
+  	marginBottom: 0,
+  	borderBottomLeftRadius: 15,
+  	borderBottomRightRadius: 15,
+  	
   },
   title:{
   	fontFamily: "Roboto-Bold",
-  	fontSize: 16,
-    textAlign: 'left',
+  	fontSize: 22,
+    textAlign: 'right',
+  },
+  artist:{
+  	fontFamily: "Roboto-Regular",
+  	fontSize: 18,
+    textAlign: 'right',
+  },
+  album:{
+  	fontFamily: "Roboto-Regular",
+  	fontSize: 18,
+    textAlign: 'right',
+  },
+  image:{
+	width: 120, 
+	height: 120,
+	marginLeft: 'auto',
   }
 });
